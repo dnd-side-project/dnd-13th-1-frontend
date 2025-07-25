44c4e14f6125055df6f9cbe58b9f902a44934729
woongaaaa <woonge0930@naver.com>
2025-07-25 19:56:28 +0900

    #1 - 프로젝트 파일 생성

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by 황채웅 on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
