314046c138be37cf477008d6fca0b414f6d37d94
minsolkim <123alsthf@naver.com>
2025-08-08 00:09:09 +0900

    #10 - ViewModel 프로토콜 추가
    ObservableObject 프로토콜 상속하여 SwiftUI 바인딩 지원

---
+++ AlloPresentation/Sources/AlloPresentation/Protocol/ViewModelable.swift
@@ -0,0 +1,16 @@
+//
+//  ViewModelable.swift
+//  AlloPresentation
+//
+//  Created by 김민솔 on 8/6/25.
+//
+
+import SwiftUI
+import Combine
+
+protocol ViewModelable: ObservableObject {
+    associatedtype Action
+    associatedtype State
+    var state: State { get }
+    func action(_ action: Action)
+}
