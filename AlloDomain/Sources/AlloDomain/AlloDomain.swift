3b4ab0c2c1d6568c369f050c49062e934faa160b
woongaaaa <woonge0930@naver.com>
2025-08-08 14:04:18 +0900

    #12 - 도메인 레이어 패키지 의존성 추가

--- AlloDomain/Sources/AlloDomain/AlloDomain.swift
+++ AlloDomain/Sources/AlloDomain/AlloDomain.swift
@@ -1,2 +1,12 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public enum DayOfTheWeek: String, CaseIterable {
+    case monday = "월"
+    case tuesday = "화"
+    case wednesday = "수"
+    case thursday = "목"
+    case friday = "금"
+    case saturday = "토"
+    case sunday = "일"
+}
