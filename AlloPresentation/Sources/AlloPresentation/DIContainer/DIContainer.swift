d86199f43f7c7738768f963f0e7af6139a39ba92
minsolkim <123alsthf@naver.com>
2025-08-15 15:42:20 +0900

    #21 - DIContainer에 checklist usecase 추가

--- AlloPresentation/Sources/AlloPresentation/DIContainer/DIContainer.swift
+++ AlloPresentation/Sources/AlloPresentation/DIContainer/DIContainer.swift
@@ -7,8 +7,16 @@
 
 import Foundation
 import SwiftUI
+import AlloData
+import AlloDomain
 
 public protocol DIContainer {
     // MARK: - Auth UseCases
     // MARK: - User UseCases
+    // MARK: - CheckList UseCases
+    func resolveGenerateCalendarDateUseCase() -> GenerateCalendarDateUseCase
+    func resolveCompleteHouseworkUseCase() -> CompleteHouseworkUseCase
+    func resolveDeleteHouseworkUseCase() -> DeleteHouseworkUseCase
+    func resolveGetHouseworkListUseCase() -> GetHouseworkListUseCase
+    func resolveGetHaveHouseworkUseCase() -> GetHaveHouseworkUseCase
 }
