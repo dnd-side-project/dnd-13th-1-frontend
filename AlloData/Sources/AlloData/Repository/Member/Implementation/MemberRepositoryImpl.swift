3d56a2da942a8c2a30f219bc767b28c539a40a46
minsolkim <123alsthf@naver.com>
2025-08-21 00:28:53 +0900

    #23 - member usecase 및 repository 생성

---
+++ AlloData/Sources/AlloData/Repository/Member/Implementation/MemberRepositoryImpl.swift
@@ -0,0 +1,17 @@
+//
+//  MemberRepositoryImpl.swift
+//  AlloData
+//
+//  Created by 김민솔 on 8/21/25.
+//
+
+import Foundation
+import AlloDomain
+
+final class MemberRepositoryImpl: MemberRepository {
+    // TODO: Service 의존성 추가
+    func fetchMembers() async throws -> [Member] {
+        return []
+    }
+}
+
