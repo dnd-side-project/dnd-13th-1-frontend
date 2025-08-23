5fe7c3e4ae8bc66585d867cd5200efaa2506d6c3
minsolkim <123alsthf@naver.com>
2025-08-23 13:33:10 +0900

    #29 - HouseworkDaysRepositoryImpl 추가

---
+++ AlloData/Sources/AlloData/Repository/Emotion/Implementation/HouseworkDaysRepositoryImpl.swift
@@ -0,0 +1,17 @@
+//
+//  HouseworkDaysRepositoryImpl.swift
+//  AlloData
+//
+//  Created by 김민솔 on 8/23/25.
+//
+
+import Foundation
+import AlloDomain
+
+final class HouseworkDaysRepositoryImpl: HouseworkDaysRepository {
+    // TODO: Service 의존성 추가
+    public func fetchSevenDaysHousework() async throws -> [HouseworkSevendays] {
+        return [
+        ]
+    }
+}
